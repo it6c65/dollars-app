73e70c9c20ddbf58591f231963eb4c532f2facdf
Luis Ilarraza <ilarrazaluis82@gmail.com>
2020-04-17 12:22:05 -0400

    Updates

---
+++ precache-manifest.92bd8ddabcb9013252196d50457fb2e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "e03678e4ba0552a74760",
+    "url": "/bundle.e0367.js"
+  },
+  {
+    "revision": "e03678e4ba0552a74760",
+    "url": "/bundle.f74b3.css"
+  },
+  {
+    "revision": "d8968b6045fc51169630ba82bd44d906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a596390ebcee0e66821",
+    "url": "/polyfills.1a596.js"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "c5be882a84fb4d465a559e84ea45a4fd",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
---
+++ precache-manifest.a72baf537453bc314e34f22e15096d39.esm.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "e03678e4ba0552a74760",
+    "url": "/bundle.4be5e.esm.js"
+  },
+  {
+    "revision": "e03678e4ba0552a74760",
+    "url": "/bundle.f74b3.css"
+  },
+  {
+    "revision": "d8968b6045fc51169630ba82bd44d906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a596390ebcee0e66821",
+    "url": "/polyfills.851f4.esm.js"
+  }
+]);
